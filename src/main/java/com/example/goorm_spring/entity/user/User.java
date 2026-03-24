6db77ab3737b75be18c35a0a0751f1745f0688c1
lee0806 <leeseh0806@gmail.com>
2026-03-24 14:59:46 +0900

    [STUDY] User 엔티티 정의
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

---
+++ src/main/java/com/example/goorm_spring/entity/user/User.java
@@ -0,0 +1,24 @@
+package com.example.goorm_spring.entity.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity // DB 테이블과 매핑되는 클래스
+@Table(name = "users") // 테이블 이름
+@Getter // 모든 필드의 getter를 자동 생성
+@NoArgsConstructor // 기본 생성자 자동 생성
+@AllArgsConstructor // @Builder가 쓸 전체 생성자를 자동 생성
+@Builder
+public class User {
+    @Id // PK 컬럼
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENT: 자동 증가
+    private Long id;
+
+    @Column(nullable = false, length = 50) // NOT NULL 조건과 최대 50자
+    private String name;
+
+    @Column(nullable = false, unique = true) // NOT NULL 조건과 중복 불가
+    private String email;
+
+}
